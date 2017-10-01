de209c3995d4d06ca0c46366ef96436a1e2eaf52
João Delgado <joaomtdelgado@gmail.com>
2017-10-01 17:05:29 +0100

    Read rom for command line arguments

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub struct Config {
+    pub rom_name: String,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        // Skip program name
+        args.next();
+
+        let rom_name = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Please provide the rom name the first argument"),
+        };
+
+        Ok(Config { rom_name })
+    }
+}
+
+pub fn read_rom(config: &Config) -> Result<Vec<u8>, Box<Error>> {
+    let mut file = File::open(&config.rom_name)?;
+    let mut data: Vec<u8> = Vec::new();
+
+    file.read_to_end(&mut data)?;
+
+    Ok(data)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate rustboy;
+
+use std::env;
+use std::process;
+
+use rustboy::Config;
+
+fn to_hex_string(bytes: &[u8]) -> String {
+    let strs: Vec<String> = bytes.iter().map(|b| format!("{:02X}", b)).collect();
+    strs.join(" ")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|e| {
+        eprintln!("Error parsing arguments: {}", e);
+        process::exit(1);
+    });
+
+    let data = rustboy::read_rom(&config).unwrap_or_else(|e| {
+        eprintln!("Error reading rom: {}", e);
+        process::exit(1);
+    });
+
+    println!("Read {} bytes", data.len());
+    println!("First 50 bytes: {}", to_hex_string(&data[0x100..0x110]));
 }
