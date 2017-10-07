a337779e49a0db0d5340dd26217db8de588c05b7
João Delgado <joaomtdelgado@gmail.com>
2017-10-07 15:47:37 +0100

    Add cartridge to GameBoy

--- src/game_boy.rs
+++ src/game_boy.rs
@@ -1,18 +1,28 @@
+#![allow(dead_code)]
+
+use std::error::Error;
+
+use cpu::Cpu;
+use cartridge::Cartridge;
+use Config;
+
 const RAM_SIZE: usize = 32 * 1024;
 const VRAM_SIZE: usize = 16 * 1024;
 
 struct GameBoy {
     cpu: Cpu,
+    cartridge: Cartridge,
     ram: [u8; RAM_SIZE],
     vram: [u8; VRAM_SIZE],
 }
 
 impl GameBoy {
-    pub fn new() -> GameBoy {
-        GameBoy {
+    pub fn new(config: &Config) -> Result<GameBoy, Box<Error>> {
+        Ok(GameBoy {
             cpu: Cpu::new(),
+            cartridge: Cartridge::new(config)?,
             ram: [0; RAM_SIZE],
             vram: [0; VRAM_SIZE],
-        }
+        })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod cpu;
+mod game_boy;
 pub mod cartridge;
 pub mod utils;
 
